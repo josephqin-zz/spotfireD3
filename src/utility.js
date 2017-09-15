aedf2eb7acf4b301fcca2c094324e6b924c071b6
Joseph Qin <Joseph.Qin@agios.com>
2017-09-15 18:46:45 -0400

    update utility.js

---
+++ src/utility.js
@@ -0,0 +1,40 @@
+'use strict'
+window.agios = (function(){
+	var exports = {
+		version:'0.1',
+		author:'joseph',
+		required:'d3V4'
+	};
+    
+    //get Max and Min of given array;
+	exports.getScale = function(vals){
+			return vals.reduce( (acc,cur) => {return {max:Math.max(acc.max,+cur),min:Math.min(acc.min,+cur)} } ,{max:+vals[0],min:+vals[0]} )
+		};
+
+	//return d3.nest() by given the hierachical groups structure
+	exports.groupBy = function(groups){
+		let nest = d3.nest();
+		groups.map((f)=>((d)=>d[f])).forEach((d)=>nest.key(d))
+		return nest.sortKeys(d3.ascending);
+	};
+    
+    //get unique values array of give group name list
+    exports.groupValue = function(dataset,groups){
+    	return groups.map((d)=>dataset.map((s)=>s[d]).filter((v,i,self)=>self.indexOf(v)===i))
+    } 
+    //build the d3.nest structure frame
+    exports.groupTree = function(groups){
+    	return [...groups].reverse()
+    					  .reduce((acc,d)=>d.reduce((a,g)=>{a.push({key:g}); return a},[]).map((k)=>{k.values=[...acc];return k}),[])
+    }
+    //
+    exports.groupFrame = function(groups){
+    	return groups.reduce((acc,d)=>{ 
+    				 let acclength = acc.length>0?d.length*acc[acc.length-1].length:d.length; 
+    				 acc.push(d3.range(acclength).map((i)=>d[i%d.length])); 
+    				 return acc; 
+    				},[])
+    }
+
+	return exports;
+})()
